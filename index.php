3f496d3dcc498942efddadb55417a52d5a9acba5
Dan0n007 <Kvetot@gmail.com>
2021-10-14 14:44:28 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,8 @@
+<form action="index.php" method="post">
+    <p>Называние фильма : <input type="text" name="name" /></p>
+    <p>дата выхода: <input type="text" name="ageyear" /></p>
+    <p>бюджет: <input type="text" name="sel" /></p>
+    <p>режисер: <input type="text" name="prod" /></p>
+
+    <p><input type="submit" /></p>
+</form>
