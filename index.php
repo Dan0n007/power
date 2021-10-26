4a0ebd8322823eedd45f2474a49f7127e3c28afb
Dan0n007 <Kvetot@gmail.com>
2021-10-26 22:23:16 +0300

    add_style

--- index.php
+++ index.php
@@ -1,8 +1,11 @@
-<form action="" method="post">
-    <p>Называние фильма : <input type="text" name="name" /></p>
-    <p>дата выхода: <input type="text" name="ageyear" /></p>
-    <p>бюджет: <input type="text" name="sel" /></p>
-    <p>режисер: <input type="text" name="prod" /></p>
 
+<link rel='stylesheet' href='style.css'>
+<form action="" method="post">
+
+    <p>Называние фильма : <input type="text" name="name" /></p>
+    <p>Дата выхода: <input type="text" name="ageyear" /></p>
+    <p>Бюджет: <input type="text" name="sel" /></p>
+    <p>Режисер: <input type="text" name="prod" /></p>
     <p><input type="submit" /></p>
+
 </form>
